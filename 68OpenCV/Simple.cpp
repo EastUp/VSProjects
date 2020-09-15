a5a3d8619b19ff7cd8b73770f217f57ae16bcba4
GitHub EastUp <GitHub 631820188@qq.com>
2020-09-15 15:54:17 +0800

    二维码识别

--- 68OpenCV/Simple.cpp
+++ 68OpenCV/Simple.cpp
@@ -174,7 +174,7 @@ bool isYCorner(const Mat& qrROI){
 }
 
 
-int main(){
+/*int main(){
 	Mat src = imread("E:/qcode1.jpg");
 	if (!src.data){
 		cout << "read error" << endl;
@@ -217,6 +217,95 @@ int main(){
 	}
 
 	imshow("dstsrc",src);
+
+	waitKey(0);
+	return 0;
+}*/
+
+int main(){
+	Mat src = imread("E:/qcode3.webp");
+	if (!src.data){
+		cout << "read error" << endl;
+		return -1;
+	}
+
+	imshow("src", src);
+
+	// 对图像进行灰度转换
+	Mat gary;
+	if (src.type() == CV_8UC4)
+		cvtColor(src, gary, COLOR_BGRA2GRAY);
+	else
+		cvtColor(src, gary, COLOR_BGR2GRAY);
+
+	// 二值化
+	threshold(gary, gary, 0, 255, THRESH_BINARY | THRESH_OTSU);
+
+	// 1. 对其进行轮廓查找
+	vector<Vec4i> hierarchy;
+	vector<vector<Point> > contours;
+	vector<vector<Point> > contoursRes;
+	/*
+	参数说明：https://blog.csdn.net/guduruyu/article/details/69220296
+	输入图像image必须为一个2值单通道图像
+
+	contours参数为检测的轮廓数组，每一个轮廓用一个point类型的vector表示
+
+	hiararchy参数和轮廓个数相同，每个轮廓contours[ i ]对应4个hierarchy元素hierarchy[ i ][ 0 ] ~hierarchy[ i ][ 3 ]，
+	分别表示后一个轮廓、前一个轮廓、父轮廓、内嵌轮廓的索引编号，如果没有对应项，该值设置为负数。
+
+	mode表示轮廓的检索模式
+	CV_RETR_EXTERNAL 表示只检测外轮廓
+	CV_RETR_LIST 检测的轮廓不建立等级关系
+	CV_RETR_CCOMP 建立两个等级的轮廓，上面的一层为外边界，里面的一层为内孔的边界信息。如果内孔内还有一个连通物体，这个物体的边界也在顶层。
+	CV_RETR_TREE 建立一个等级树结构的轮廓。具体参考contours.c这个demo
+
+	method为轮廓的近似办法
+	CV_CHAIN_APPROX_NONE 存储所有的轮廓点，相邻的两个点的像素位置差不超过1，即max（abs（x1-x2），abs（y2-y1））==1
+	CV_CHAIN_APPROX_SIMPLE 压缩水平方向，垂直方向，对角线方向的元素，只保留该方向的终点坐标，例如一个矩形轮廓只需4个点来保存轮廓信息
+	CV_CHAIN_APPROX_TC89_L1，CV_CHAIN_APPROX_TC89_KCOS 使用teh-Chinl chain 近似算法
+	offset表示代表轮廓点的偏移量，可以设置为任意值。对ROI图像中找出的轮廓，并要在整个图像中进行分析时，这个参数还是很有用的。
+	*/
+	findContours(gary, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE, Point(0, 0));
+	int tCC = 0; // 临时用来累加的子轮廓计数器
+	int pId = -1;// 父轮廓的 index
+	for (int i = 0; i < contours.size(); i++) {
+		if (hierarchy[i][2] != -1 && tCC == 0) {
+			pId = i;
+			tCC++;
+		}
+		else if (hierarchy[i][2] != -1) {// 有父轮廓
+			tCC++;
+		}
+		else if (hierarchy[i][2] == -1) {// 没有父轮廓
+			tCC = 0;
+			pId = -1;
+		}
+		// 找到了两个子轮廓
+		if (tCC >= 2) {
+			contoursRes.push_back(contours[pId]);
+			tCC = 0;
+			pId = -1;
+		}
+	}
+
+
+
+	// 找到过多的符合特征轮廓，对其进行筛选
+	//if (contoursRes.size() > FEATURE_NUMBER) {
+	//	contoursRes = filterContours(gary, contoursRes);
+	//}
+
+	// 没有找到符合的条件
+	//if (contoursRes.size() < FEATURE_NUMBER) {
+	//	return NULL;
+	//}
+
+	for (int i = 0; i < contoursRes.size(); ++i) {
+		drawContours(src, contoursRes, i, Scalar(0, 0, 255), 2);
+	}
+
+	imshow("dstSrc",src);
 
 	waitKey(0);
 	return 0;
